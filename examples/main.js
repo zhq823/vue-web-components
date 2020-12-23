2bf0cf2d9b698ca20c6a19877d506d6880421aa2
Jon Snow <885479297@qq.com>
2020-12-23 10:51:39 +0800

    初版

--- examples/main.js
+++ examples/main.js
@@ -7,6 +7,11 @@ import store from "./store";
 import VueWebComponents from '../packages/index';
 Vue.use(VueWebComponents);
 
+// 引入npm包
+// import VueWebComponents from 'vue-web-components';
+// import 'vue-web-components/lib/vue-web-components.css';
+// Vue.use(VueWebComponents)
+
 Vue.config.productionTip = false;
 
 new Vue({
